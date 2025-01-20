cad614f056671462f98243f3c2638ff0db7a76cd
hoaduy994 <58455712+hoaduy994@users.noreply.github.com>
2025-01-20 17:14:03 +0700

    Delete user_service directory

--- user_service/UserAuth/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('register/', UserRegiste.as_view(), name='register'),
-    path('login/', Login.as_view(), name='login'),
-    path('token/refresh/', RefreshTokenView.as_view(), name='token-refresh'),
-]
