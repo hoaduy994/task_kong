fdc3a0cc0e285dded3e1644166d8148a6ca7344c
hoaduy994 <58455712+hoaduy994@users.noreply.github.com>
2025-01-20 17:13:54 +0700

    Delete blog_service directory

--- blog_service/blog/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('', BlogList.as_view(), name='blog-list'),
-    path('create/', BlogCreate.as_view(), name='blog-create'),
-    path('<int:pk>/update/', BlogUpdate.as_view(), name='blog-update'),
-    path('<int:pk>/delete/', BlogDelete.as_view(), name='blog-delete'),
-]
